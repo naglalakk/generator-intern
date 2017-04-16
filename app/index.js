bb136f08a61dd6574beb8fb5c332706a804f093e
Thordur Hermannsson <doddi@kott.is>
2017-04-16 11:58:44 +0000

    updating generator version

--- app/index.js
+++ app/index.js
@@ -1,10 +1,10 @@
 'use strict';
-var yeoman = require('yeoman-generator');
+var Base = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 var path  = require('path');
 
-module.exports = yeoman.generators.Base.extend({
+module.exports = Base.extend({
   initializing: function () {
   },
 
